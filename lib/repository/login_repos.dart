380bfb65352bb4f1bb0842a58f83ced83264e8c2
Wichada Jamjai <wichada.jam@pandasoft.co.th>
2023-05-31 17:41:10 +0700

    [EL-51] Integrate register profile and login

---
+++ lib/repository/login_repos.dart
@@ -0,0 +1,39 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:health_application/repository/service/login_client.dart';
+
+import 'package:retrofit/dio.dart';
+
+import '../ui/base/model/failure.dart';
+import '../ui/base/model/status_code.dart';
+import '../ui/base/network_provider.dart';
+import '../ui/signIn_page/model/login_model.dart';
+import '../ui/signIn_page/model/sign_in_model.dart';
+
+class LoginRepository {
+  LoginRepository();
+
+  final NetworkProvider networkProvider = NetworkProvider();
+  late final LoginService _loginService =
+      LoginService(networkProvider.dioClient());
+
+  Future<Either<Failure, LoginModel>> login(SignInModel body) async {
+    try {
+      final HttpResponse req = await _loginService.login(body.toJson());
+      final Map<String, dynamic> data = req.data;
+      if (req.response.statusCode == StatusCode.success) {
+        return Right(LoginModel.fromJson(data['data']));
+      }
+    } on DioError catch (error) {
+      print(error.response);
+      if (error.response?.statusCode == StatusCode.notFound) {
+        print('Error 400 $error');
+        return const Left(Failure(''));
+      } else if (error.response?.statusCode == StatusCode.failure) {
+        print('Error 500 $error');
+        return const Left(Failure(''));
+      }
+    }
+    return const Left(Failure(''));
+  }
+}
