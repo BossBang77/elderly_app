833cb0fcc5c36276cc990e764432e8d9a7f31f7f
Wichada Jamjai <wichada.jam@pandasoft.co.th>
2023-06-07 18:46:07 +0700

    [EL-87][EL-88][EL-89][EL-90][EL-91]  wrap bloc

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:health_application/ui/welcome_page/welcome_page.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 import 'ui/elderly/exercise/bloc/exercise_bloc.dart';
+import 'ui/elderly/water_intake/bloc/water_intake_bloc.dart';
 import 'ui/home_page/bloc/home_page_bloc.dart';
 
 void main() {
@@ -41,6 +42,8 @@ class MyApp extends StatelessWidget {
               BlocProvider(create: (context) => HomePageBloc()),
               BlocProvider(create: (context) => ExerciseBloc()),
               BlocProvider(create: (context) => MasterDataBloc()),
+              BlocProvider(
+                  create: (context) => WaterIntakeBloc()..add(Initial())),
             ],
             child: Builder(builder: (context) {
               context.read<MasterDataBloc>().add(LoadMasterData());
