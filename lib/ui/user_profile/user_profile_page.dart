456b965c7d160352da1f76d295523126fe227753
Wichada Jamjai <wichada.jam@pandasoft.co.th>
2023-06-08 11:21:18 +0700

    [EL-103] add ui user profile display by role

--- lib/ui/user_profile/user_profile_page.dart
+++ lib/ui/user_profile/user_profile_page.dart
@@ -3,6 +3,9 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:health_application/ui/user_profile/bloc/user_profile_bloc.dart';
 import 'package:health_application/ui/user_profile/elderly_profile.dart';
+import 'package:health_application/ui/user_profile/volunteer_profile.dart';
+
+import '../register_profile/bloc/register_profile_bloc.dart';
 
 class UserProfilePage extends StatelessWidget {
   const UserProfilePage({super.key});
@@ -12,7 +15,13 @@ class UserProfilePage extends StatelessWidget {
     return BlocConsumer<UserProfileBloc, UserProfileState>(
         listener: (context, state) async {},
         builder: (BuildContext context, UserProfileState state) {
-          return ElderProfileWidget(state: state);
+          if (state.userProfile.role == RoleType.ROLE_USER_ELDERLY.name) {
+            return ElderProfileWidget(state: state);
+          } else {
+            return VolunteerProfileWidget(
+              state: state,
+            );
+          }
         });
   }
 }
