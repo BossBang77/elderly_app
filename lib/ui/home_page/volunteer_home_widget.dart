10ddad716cd867f40927076edf3ca01e95d36e89
Panomkorn Noknoi <panomkorn@pandasoft.co.th>
2023-06-12 18:33:17 +0700

    [EL-133] set read user profile for display

--- lib/ui/home_page/volunteer_home_widget.dart
+++ lib/ui/home_page/volunteer_home_widget.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:health_application/ui/extension/date_extension.dart';
 import 'package:health_application/ui/home_page/component/appointment_item.dart';
 import 'package:health_application/ui/home_page/component/volunteer_menu.dart';
 import 'package:health_application/ui/ui-extensions/color.dart';
 import 'package:health_application/ui/ui-extensions/font.dart';
+import 'package:health_application/ui/user_profile/bloc/user_profile_bloc.dart';
 
 class VolunteerHomeWidget extends StatelessWidget {
   const VolunteerHomeWidget({super.key});
@@ -25,85 +27,97 @@ class VolunteerHomeWidget extends StatelessWidget {
                   fit: BoxFit.fitWidth,
                 ),
               ),
-              Positioned(
-                top: 90,
-                left: 20,
-                right: 20,
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  child: Column(
-                    children: [
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            children: [
-                              Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  textSubtitle24(
-                                      'สวัสดี, นางทองใบ', ColorTheme().white),
-                                  textSubtitle18Blod(
-                                      'วันนี้, ${DateTime.now().toDisplayFullBuddishDate(locale: 'th')}',
-                                      ColorTheme().white),
-                                ],
-                              ),
-                            ],
-                          ),
-                          Image.asset(
-                            true
-                                ? 'assets/images/notify_alert.png'
-                                : 'assets/images/notify.png',
-                            scale: 4,
-                          )
-                        ],
-                      ),
-
-                      SizedBox(height: 18,),
-
-                      Container(
-                        padding: EdgeInsets.fromLTRB(16, 12, 16, 10),
-                        decoration: BoxDecoration(
-                          color: Colors.black.withAlpha(51),
-                          borderRadius: BorderRadius.circular(8)
-                        ),
-                        child: Expanded(
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Container(
-                                child: Image.asset(
-                                  'assets/images/home.png',
-                                  width: 18,
-                                  height: 18,
+              BlocConsumer<UserProfileBloc, UserProfileState>(
+                listener: (context, state) {},
+                builder: (BuildContext context, UserProfileState state) {
+                  var userProfile = state.userProfile;
+                  return Positioned(
+                    top: 90,
+                    left: 20,
+                    right: 20,
+                    child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        child: Column(
+                          children: [
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        SizedBox(
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width /
+                                              1.8,
+                                          child: textSubtitle24(
+                                              'สวัสดี, ${state.status == UserProfileStatus.success ? userProfile.profile.name : ''}',
+                                              ColorTheme().white),
+                                        ),
+                                        textSubtitle18Blod(
+                                            'วันนี้, ${DateTime.now().toDisplayFullBuddishDate(locale: 'th')}',
+                                            ColorTheme().white),
+                                      ],
+                                    ),
+                                  ],
                                 ),
-                              ),
-                              SizedBox(width: 16),
-                              Expanded(
-                                child: Text(
-                                  '3000 ถ.พหลโยธิน แขวง จอมพล เขตจตุจักร 3000 ถ.พหลโยธิน แขวง จอมพล เขตจตุจักร', 
-                                  overflow: TextOverflow.ellipsis,
-                                  style: TextStyle(
-                                    fontFamily: fontFamily,
-                                    fontSize: 18.sp,
-                                    color: ColorTheme().white
-                                  ),
+                                Image.asset(
+                                  true
+                                      ? 'assets/images/notify_alert.png'
+                                      : 'assets/images/notify.png',
+                                  scale: 4,
                                 )
-                              ),
-                              GestureDetector(
-                                onTap: (){},
-                                child: Image.asset('assets/images/volunteer_chevron_right.png', width: 20, height: 20)
-                              )
-                              // textSubtitle1('3000 ถ.พหลโยธิน แขวง จอมพล เขตจตุจักร ', ColorTheme().white)
-                            ],
-                          ),
-                        )
-                      )
-                    ],
-                  )
-                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 18,
+                            ),
+                            Container(
+                                padding: EdgeInsets.fromLTRB(16, 12, 16, 10),
+                                decoration: BoxDecoration(
+                                    color: Colors.black.withAlpha(51),
+                                    borderRadius: BorderRadius.circular(8)),
+                                child: Expanded(
+                                  child: Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Container(
+                                        child: Image.asset(
+                                          'assets/images/home.png',
+                                          width: 18,
+                                          height: 18,
+                                        ),
+                                      ),
+                                      SizedBox(width: 16),
+                                      Expanded(
+                                          child: Text(
+                                        '3000 ถ.พหลโยธิน แขวง จอมพล เขตจตุจักร 3000 ถ.พหลโยธิน แขวง จอมพล เขตจตุจักร',
+                                        overflow: TextOverflow.ellipsis,
+                                        style: TextStyle(
+                                            fontFamily: fontFamily,
+                                            fontSize: 18.sp,
+                                            color: ColorTheme().white),
+                                      )),
+                                      GestureDetector(
+                                          onTap: () {},
+                                          child: Image.asset(
+                                              'assets/images/volunteer_chevron_right.png',
+                                              width: 20,
+                                              height: 20))
+                                      // textSubtitle1('3000 ถ.พหลโยธิน แขวง จอมพล เขตจตุจักร ', ColorTheme().white)
+                                    ],
+                                  ),
+                                ))
+                          ],
+                        )),
+                  );
+                },
               ),
               Container(
                 margin: EdgeInsets.only(top: 222),
@@ -119,25 +133,24 @@ class VolunteerHomeWidget extends StatelessWidget {
                       Row(
                         children: [
                           Expanded(
-                            child: VolunteerMenu(
-                              title: 'รายการอาหาร',
-                              image: 'assets/images/volunteer_food_bg.png',
-                            )
-                          ),
+                              child: VolunteerMenu(
+                            title: 'รายการอาหาร',
+                            image: 'assets/images/volunteer_food_bg.png',
+                          )),
                           SizedBox(width: 20),
                           Expanded(
-                            child: VolunteerMenu(
-                              title: 'การออกกำลังกาย',
-                              image: 'assets/images/volunteer_exercise_bg.png',
-                            )
-                          )
+                              child: VolunteerMenu(
+                            title: 'การออกกำลังกาย',
+                            image: 'assets/images/volunteer_exercise_bg.png',
+                          ))
                         ],
                       ),
-                      
                       const SizedBox(
                         height: 10,
                       ),
-                      VolunteerMenu(title: 'ค้นหาข้อมูลผู้สูงอายุ', image: 'assets/images/volunteer_elder_bg.png'),
+                      VolunteerMenu(
+                          title: 'ค้นหาข้อมูลผู้สูงอายุ',
+                          image: 'assets/images/volunteer_elder_bg.png'),
                       const SizedBox(
                         height: 20,
                       ),
@@ -158,11 +171,9 @@ class VolunteerHomeWidget extends StatelessWidget {
                       const SizedBox(
                         height: 10,
                       ),
-                      
                       const SizedBox(
                         height: 10,
                       ),
-                      
                       const SizedBox(
                         height: 100,
                       ),
