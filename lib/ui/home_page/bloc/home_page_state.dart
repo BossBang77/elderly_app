ea2462bd5b7039a692ba331f6484b5fc1ebbc6a3
Wichada Jamjai <wichada.jam@pandasoft.co.th>
2023-06-14 18:43:06 +0700

    set get role to display home page

--- lib/ui/home_page/bloc/home_page_state.dart
+++ lib/ui/home_page/bloc/home_page_state.dart
@@ -11,17 +11,21 @@ enum menuType {
 }
 
 class HomePageState extends Equatable {
-  const HomePageState({this.menus = menuType.mainPage});
+  const HomePageState(
+      {this.menus = menuType.mainPage, this.role = '', this.loading = false});
   final menuType menus;
+  final String role;
+  final bool loading;
 
-  HomePageState copyWith({
-    menuType? menus,
-  }) {
-    return HomePageState(menus: menus ?? this.menus);
+  HomePageState copyWith({menuType? menus, String? role, bool? loading}) {
+    return HomePageState(
+        menus: menus ?? this.menus,
+        role: role ?? this.role,
+        loading: loading ?? this.loading);
   }
 
   @override
-  List<Object> get props => [menus];
+  List<Object> get props => [menus, role, loading];
 }
 
 class HomePageInitial extends HomePageState {}
