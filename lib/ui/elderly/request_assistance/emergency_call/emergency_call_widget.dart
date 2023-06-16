a24eb88c132e71caf9f62cb43497a41c8d28a23f
Wichada Jamjai <wichada.jam@pandasoft.co.th>
2023-06-16 12:29:33 +0700

    [EL-158] Set gif emergency

--- lib/ui/elderly/request_assistance/emergency_call/emergency_call_widget.dart
+++ lib/ui/elderly/request_assistance/emergency_call/emergency_call_widget.dart
@@ -4,6 +4,7 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:health_application/ui/base/widget/app_bar_widget.dart';
+import 'package:health_application/ui/base/widget/back_button.dart';
 import 'package:health_application/ui/elderly/request_assistance/bloc/request_assistance_bloc.dart';
 import 'package:health_application/ui/home_page/home_page.dart';
 import 'package:health_application/ui/ui-extensions/color.dart';
@@ -17,50 +18,64 @@ class EmergrncyCallWidget extends StatelessWidget {
     var sized = MediaQuery.of(context).size;
     return Scaffold(
         backgroundColor: color.white,
-        appBar: appBar(
-            onBack: () {
-              Navigator.of(context).pushAndRemoveUntil(
-                  MaterialPageRoute(builder: (context) => HomePage()),
-                  (Route<dynamic> route) => false);
-            },
-            title: ''),
-        body: Container(
-          width: sized.width,
-          height: sized.height,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 20,
+        body: Stack(
+          children: [
+            Positioned.fill(
+              child: Image.asset(
+                'assets/images/emergency_icon.gif',
+                fit: BoxFit.cover,
               ),
-              textSubtitle18Blod('Emergency call', color.emergency),
-              const SizedBox(
-                height: 20,
+            ),
+            Container(
+              width: sized.width,
+              height: sized.height,
+              margin: EdgeInsets.only(top: 100),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Column(
+                    children: [
+                      textSubtitle18Blod('Emergency call', color.emergency),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      textSubtitle24W700(
+                          'คุณต้องการ\nความช่วยเหลือหรือไม่?', color.black87,
+                          align: TextAlign.center),
+                    ],
+                  ),
+                  InkWell(
+                      onTap: () {
+                        context.read<RequestAssistanceBloc>().add(UpdateStatus(
+                            status: AssistanceStatus.waitingForHelp));
+                      },
+                      child: Container(
+                        padding: EdgeInsets.all(150),
+                      )),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  textSubtitle16Blod(
+                      'กดปุ่มเพื่อขอความช่วยเหลือฉุกเฉิน', color.black87),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                ],
               ),
-              textSubtitle24W700(
-                  'คุณต้องการ\nความช่วยเหลือหรือไม่?', color.black87,
-                  align: TextAlign.center),
-              InkWell(
-                onTap: () {
-                  context.read<RequestAssistanceBloc>().add(
-                      UpdateStatus(status: AssistanceStatus.waitingForHelp));
+            ),
+            Positioned(
+              top: 70,
+              left: 30,
+              child: BackButtonWidget(
+                onClick: () {
+                  Navigator.of(context).pushAndRemoveUntil(
+                      MaterialPageRoute(builder: (context) => HomePage()),
+                      (Route<dynamic> route) => false);
                 },
-                child: Image.asset(
-                  'assets/images/emergency_icon.png',
-                  scale: 3.5,
-                ),
               ),
-              const SizedBox(
-                height: 10,
-              ),
-              textSubtitle16Blod(
-                  'กดปุ่มเพื่อขอความช่วยเหลือฉุกเฉิน', color.black87),
-              const SizedBox(
-                height: 20,
-              ),
-            ],
-          ),
+            ),
+          ],
         ));
   }
 }
