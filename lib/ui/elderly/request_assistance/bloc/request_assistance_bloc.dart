fb64d9d87158e6c9f46a9d9e88275fd22d08f160
Wichada Jamjai <wichada.jam@pandasoft.co.th>
2023-06-19 17:29:08 +0700

    [EL-176][EL-177][EL-178] integrate and set state of widget of emergency function elderly

--- lib/ui/elderly/request_assistance/bloc/request_assistance_bloc.dart
+++ lib/ui/elderly/request_assistance/bloc/request_assistance_bloc.dart
@@ -1,5 +1,12 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:health_application/repository/emergency_repos.dart';
+import 'package:health_application/ui/base/model/failure.dart';
+import 'package:health_application/ui/elderly/request_assistance/model/request/create_emergency_model.dart';
+import 'package:health_application/ui/elderly/request_assistance/model/request/update_status_emergency_model.dart';
+import 'package:health_application/ui/register_profile/bloc/register_profile_bloc.dart';
+
+import '../model/reesponse/emergency_detail_model.dart';
 
 part 'request_assistance_event.dart';
 part 'request_assistance_state.dart';
@@ -7,11 +14,108 @@ part 'request_assistance_state.dart';
 class RequestAssistanceBloc
     extends Bloc<RequestAssistanceEvent, RequestAssistanceState> {
   RequestAssistanceBloc() : super(RequestAssistanceInitial()) {}
+
+  EmergencyRepository _emergencyRepository = EmergencyRepository();
   @override
   Stream<RequestAssistanceState> mapEventToState(
       RequestAssistanceEvent event) async* {
     if (event is UpdateStatus) {
       yield state.copyWith(assistanceStatus: event.status);
     }
+    if (event is SetLoading) {
+      yield state.copyWith(isLoading: event.loading);
+    }
+    if (event is CreateEmergency) {
+      yield state.copyWith(isLoading: true);
+      var res = await _emergencyRepository
+          .createEmergency(event.createEmergencyModel);
+      yield* res.fold((Failure err) async* {
+        yield state.copyWith(
+            isLoading: false,
+            assistanceStatus: AssistanceStatus.emergencyCall,
+            statusSubmit: StatusSubmit.createEmergencyFail);
+      }, (String res) async* {
+        yield state.copyWith(
+            isLoading: false,
+            assistanceStatus: AssistanceStatus.waitingForHelp);
+        add(GetEmergencyDetail());
+      });
+    }
+
+    if (event is GetEmergencyDetail) {
+      yield state.copyWith(isLoading: true, statusSubmit: StatusSubmit.initial);
+      var res = await _emergencyRepository.getEmergencyDetail();
+      yield* res.fold((Failure err) async* {
+        yield state.copyWith(
+          emergencyHelpDetail: EmergencyHelpDetailModel(),
+          assistanceStatus: AssistanceStatus.emergencyCall,
+          isLoading: false,
+        );
+      }, (EmergencyHelpDetailModel res) async* {
+        AssistanceStatus status = AssistanceStatus.waitingForHelp;
+        if (res.status == EmergencyStatus.WAITING.name) {
+          await requestUpdateEmergency();
+        }
+        if (res.emergencyHelpers.isNotEmpty) {
+          status = AssistanceStatus.volunteerAccept;
+        }
+        yield state.copyWith(
+            isLoading: false,
+            emergencyHelpDetail: res,
+            assistanceStatus: status);
+      });
+    }
+
+    if (event is SubmitIsSafe) {
+      yield state.copyWith(isLoading: true);
+      UpdateStatusEmergencyModel body = UpdateStatusEmergencyModel(
+          id: state.emergencyHelpDetail.id,
+          status: EmergencyStatus.ELDERLY_COMPLETE.name);
+      var res = await _emergencyRepository.updateEmergencyStatus(body);
+      yield* res.fold((Failure err) async* {
+        yield state.copyWith(
+            isLoading: false, statusSubmit: StatusSubmit.sendSafeSuccess);
+      }, (String res) async* {
+        AssistanceStatus status = AssistanceStatus.emergencyCall;
+        yield state.copyWith(
+            isLoading: false,
+            assistanceStatus: status,
+            statusSubmit: StatusSubmit.sendSafeSuccess);
+      });
+    }
+
+    if (event is ResetSubmitStatus) {
+      yield state.copyWith(statusSubmit: StatusSubmit.initial);
+    }
+
+    if (event is UpdateEmergetcyDetail) {
+      var res = await _emergencyRepository.getEmergencyDetail();
+      yield* res.fold((Failure err) async* {
+        //TODO
+      }, (EmergencyHelpDetailModel res) async* {
+        AssistanceStatus status = AssistanceStatus.waitingForHelp;
+        if (res.status == EmergencyStatus.WAITING.name) {
+          await requestUpdateEmergency();
+        }
+        // check change data
+        if (res.emergencyHelpers.length !=
+            state.emergencyHelpDetail.emergencyHelpers.length) {
+          if (res.emergencyHelpers.isNotEmpty) {
+            status = AssistanceStatus.volunteerAccept;
+          }
+          yield state.copyWith(
+              isLoading: false,
+              emergencyHelpDetail: res,
+              assistanceStatus: status);
+        }
+      });
+    }
+  }
+
+  Future requestUpdateEmergency() async {
+    // Call new update every 10 sec
+    Future.delayed(Duration(seconds: 10), () {
+      add(UpdateEmergetcyDetail());
+    });
   }
 }
--- lib/ui/elderly/request_assistance/bloc/request_assistance_state.dart
+++ lib/ui/elderly/request_assistance/bloc/request_assistance_state.dart
@@ -2,19 +2,47 @@ part of 'request_assistance_bloc.dart';
 
 class RequestAssistanceState extends Equatable {
   const RequestAssistanceState(
-      {this.assistanceStatus = AssistanceStatus.emergencyCall});
+      {this.assistanceStatus = AssistanceStatus.initial,
+      this.isLoading = false,
+      this.emergencyHelpDetail = const EmergencyHelpDetailModel(),
+      this.statusSubmit = StatusSubmit.initial});
 
   final AssistanceStatus assistanceStatus;
+  final bool isLoading;
+  final EmergencyHelpDetailModel emergencyHelpDetail;
+  final StatusSubmit statusSubmit;
 
-  RequestAssistanceState copyWith({AssistanceStatus? assistanceStatus}) {
+  RequestAssistanceState copyWith(
+      {AssistanceStatus? assistanceStatus,
+      bool? isLoading,
+      EmergencyHelpDetailModel? emergencyHelpDetail,
+      StatusSubmit? statusSubmit}) {
     return RequestAssistanceState(
-        assistanceStatus: assistanceStatus ?? this.assistanceStatus);
+        assistanceStatus: assistanceStatus ?? this.assistanceStatus,
+        isLoading: isLoading ?? this.isLoading,
+        emergencyHelpDetail: emergencyHelpDetail ?? this.emergencyHelpDetail,
+        statusSubmit: statusSubmit ?? this.statusSubmit);
   }
 
   @override
-  List<Object> get props => [assistanceStatus];
+  List<Object> get props =>
+      [assistanceStatus, isLoading, emergencyHelpDetail, statusSubmit];
 }
 
 class RequestAssistanceInitial extends RequestAssistanceState {}
 
-enum AssistanceStatus { emergencyCall, waitingForHelp, volunteerAccept }
+enum AssistanceStatus {
+  initial,
+  emergencyCall,
+  waitingForHelp,
+  volunteerAccept
+}
+
+enum EmergencyStatus { WAITING, VOLUNTEER_COMPLETE, ELDERLY_COMPLETE }
+
+enum StatusSubmit {
+  initial,
+  sendSafeSuccess,
+  sendSafeFail,
+  createEmergencyFail
+}
