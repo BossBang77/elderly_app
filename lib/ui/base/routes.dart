3e093ba83199e474886f2e9eeaf16904b8caa408
Wichada Jamjai <wichada.jam@pandasoft.co.th>
2023-07-11 16:21:12 +0700

    [EL-357] add new routes

--- lib/ui/base/routes.dart
+++ lib/ui/base/routes.dart
@@ -20,6 +20,7 @@ import 'package:health_application/ui/register_profile/register_profile_page.dar
 import 'package:health_application/ui/signIn_page/signIn_widget.dart';
 import 'package:health_application/ui/volunteer/request_assistance_detail/request_assitance_detail_page.dart';
 import 'package:health_application/ui/user_profile/profile_information/elderly_profile_information_view.dart';
+import 'package:health_application/ui/volunteer/volunteer_address/volunteer_address_page.dart';
 import 'package:health_application/ui/volunteer/volunteer_information/volunteer_information_page.dart';
 import 'package:health_application/ui/welcome_page/welcome_page.dart';
 
@@ -195,6 +196,14 @@ final GoRouter appRouter =
           profile: userProfile,
         );
       }),
+  GoRoute(
+      path: Routes.volunteerAddress,
+      builder: (BuildContext context, GoRouterState state) {
+        var userProfile = state.extra as RegisterModel;
+        return VolunteerAddressPage(
+          profile: userProfile,
+        );
+      }),
 ]);
 
 class Routes {
@@ -248,4 +257,5 @@ class Routes {
 
   static const String changePassword = '/changePassword';
   static const String volunteerInformation = '/volunteerInformation';
+  static const String volunteerAddress = '/volunteerAddress';
 }
