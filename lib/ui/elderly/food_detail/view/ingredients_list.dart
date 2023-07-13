a1e79d5d82ce3ed9a85c84c47141582960c76b24
Wichada Jamjai <wichada.jam@pandasoft.co.th>
2023-07-13 11:28:47 +0700

    [EL-389] Fix image path

--- lib/ui/elderly/food_detail/view/ingredients_list.dart
+++ lib/ui/elderly/food_detail/view/ingredients_list.dart
@@ -47,7 +47,7 @@ class IngredientsList extends StatelessWidget {
                         onMinusButtonTap?.call();
                       },
                       child: Image.asset(numberOfPlates > 1
-                          ? 'ingredient_minus_icon_active'
+                          ? 'assets/images/ingredient_minus_icon_active.png'
                           : 'assets/images/ingredient_minus_icon.png'),
                     )),
                 SizedBox(width: 12),
