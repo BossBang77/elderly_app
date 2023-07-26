be462cfd4f869b51da8b88ddf2de5d0af7bfa9f2
Wichada Jamjai <wichada.jam@pandasoft.co.th>
2023-07-26 09:56:37 +0700

    [EL-452] set max line display multiple line

--- lib/ui/elderly/appointment_detail/appointment_detail_section/appointment_detail_item.dart
+++ lib/ui/elderly/appointment_detail/appointment_detail_section/appointment_detail_item.dart
@@ -1,31 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:health_application/ui/extension/string_extension.dart';
 import 'package:health_application/ui/ui-extensions/color.dart';
 import 'package:health_application/ui/ui-extensions/font.dart';
 
-Widget AppointmentDetailItem({
-  required String title, 
-  required String description,
-  required String image
-}) => Container(
-  child: Row(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      SizedBox(width: 12),
-      Container(
-        width: 18,
-        height: 18,
-        child: Image.asset(image, width: 18, height: 18, fit: BoxFit.contain),
+Widget AppointmentDetailItem(
+        {required String title,
+        required String description,
+        required String image}) =>
+    Container(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(width: 12),
+          Container(
+            width: 18,
+            height: 18,
+            child:
+                Image.asset(image, width: 18, height: 18, fit: BoxFit.contain),
+          ),
+          SizedBox(width: 20),
+          Expanded(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              textSubtitle2(
+                title.isNoData(),
+                ColorTheme().grey50,
+                false,
+              ),
+              textSubtitle2(description.isNoData(), ColorTheme().black87, false,
+                  maxLine: 5)
+            ],
+          ))
+        ],
       ),
-      SizedBox(width: 20),
-      Expanded(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            textSubtitle2(title, ColorTheme().grey50, false),
-            textSubtitle2(description, ColorTheme().black87, false)
-          ],
-        )
-      )
-    ],
-  ),
-);
+    );
